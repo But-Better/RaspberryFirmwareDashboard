12114a699d3050f53b4fb719c287c78b612433f6
kev <kevHTW@gmx.de>
2021-11-04 14:20:52 +0100

    init

---
+++ src/main/java/com/butbutter/firmwareDashboard/FirmwareDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.butbutter.firmwareDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmwareDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmwareDashboardApplication.class, args);
+	}
+
+}
