bef731ed150883b493071491c0049f9de05b98d7
kev <kevHTW@gmx.de>
2021-11-12 18:07:08 +0100

    read serial input

--- src/main/java/com/butbutter/firmwareDashboard/PortsController.java
+++ src/main/java/com/butbutter/firmwareDashboard/PortsController.java
@@ -18,29 +18,31 @@ public class PortsController {
     public String readPort(int id) {
         AtomicReference<String> message = new AtomicReference<>();
         new Thread(() -> {
-            SerialPort comPort = SerialPort.getCommPorts()[id];
-            comPort.openPort();
-            try {
-                int start = 0;
-                int stop = 2000;
-                while (start < stop) {
-                    while (comPort.bytesAvailable() == 0)
-                        Thread.sleep(20);
+            if (SerialPort.getCommPorts().length >= id) {
+                SerialPort comPort = SerialPort.getCommPorts()[id];
+                comPort.openPort();
+                try {
+                    int start = 0;
+                    int stop = 2000;
+                    while (start < stop) {
+                        while (comPort.bytesAvailable() == 0)
+                            Thread.sleep(20);
 
-                    byte[] readBuffer = new byte[comPort.bytesAvailable()];
-                    int numRead = comPort.readBytes(readBuffer, readBuffer.length);
-                    StringBuffer stringBuffer = new StringBuffer();
-                    stringBuffer.append(numRead);
-                    start++;
+                        byte[] readBuffer = new byte[comPort.bytesAvailable()];
+                        int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+                        StringBuffer stringBuffer = new StringBuffer();
+                        stringBuffer.append(numRead);
+                        start++;
 
-                    if (start >= stop) {
-                        message.set(stringBuffer.toString());
+                        if (start >= stop) {
+                            message.set(stringBuffer.toString());
+                        }
                     }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+                comPort.closePort();
             }
-            comPort.closePort();
         });
 
         return message.get();
